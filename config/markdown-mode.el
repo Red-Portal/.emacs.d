4c74fb87f296001860a30a85536ed0e663ce8413
Red-Portal <msca8h@naver.com>
2017-06-25 15:05:30 +0900

    formatted init.el

---
+++ config/markdown-mode.el
@@ -0,0 +1,7 @@
+(use-package markdown-mode
+  :ensure t
+  :commands(markdown-mode gfm-mode)
+  :mode(("README\\.md\\'" . gfm-mode)
+	("\\.md\\'" . markdown-mode)
+	("\\.markdown\\'" . markdwon-mode))
+  :init(setq markdown-command "multimarkdown"))
