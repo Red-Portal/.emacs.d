28982cf35bed3745a70ce48cb546bec2854915ec
Ray Kim <msca8h@naver.com>
2020-05-02 16:09:33 +0900

    moving tramp related code to separate directory

--- config/global-mode.el
+++ config/global-mode.el
@@ -15,8 +15,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(setq tramp-default-method "ssh")
-
 (leaf quelpa
   :ensure t
   :require t)
